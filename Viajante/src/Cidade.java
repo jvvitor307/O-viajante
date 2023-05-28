8f3fa9ac563a579654c4debb66c29b3b52078724
Lucas Lucena <joselucaslucena98@gmail.com>
2023-05-27 21:45:03 -0300

    Classe Cidade e Personagem

--- Viajante/src/Cidade.java
+++ Viajante/src/Cidade.java
@@ -1,4 +1,23 @@
 public abstract class Cidade{
     private String nome;
+    private int poderCidade;
+
     
+    public Cidade(String nome, int poderCidade) {
+        this.nome = nome;
+        this.poderCidade = poderCidade;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getPoderCidade() {
+        return poderCidade;
+    }
+    public void setPoderCidade(int poderCidade) {
+        this.poderCidade = poderCidade;
+    }
+
 }
---
+++ Viajante/src/Personagem.java
@@ -0,0 +1,18 @@
+public abstract class Personagem {
+    private String nome;
+    
+
+    public Personagem(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+}
